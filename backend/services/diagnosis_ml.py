fb65964c2ebf4e27fd2c64419685ffce6dd8f762
Vinay Sharma <vinayiet435@gmail.com>
2025-05-05 23:38:43 +0530

    written code for the load pickled model

--- backend/services/diagnosis_ml.py
+++ backend/services/diagnosis_ml.py
@@ -2,3 +2,23 @@ import joblib
 
 model = joblib.load('../notebooks/diagnosis_model.pkl')
 
+SYMPTOM_COLS = [
+    'fever', 'headache', 'nausea', 'vomiting', 'fatigue',
+    'joint_pain', 'skin_rash', 'cough', 'weight_loss',
+    'yellow_eyes'
+]
+
+def encode_symptoms(input_symptoms: list[str]) -> list[int]:
+    """
+    Encode the list of the symptoms into a binary feature vector.
+    """
+    return [1 if symptom in input_symptoms else 0 for symptom in SYMPTOM_COLS]
+
+def predict_disease(symptoms: list[str]) -> str:
+    """
+    Predict disease based on encoded symptoms
+    
+    """
+    features = encode_symptoms(symptoms)
+    prediction = model.predict([features])
+    return prediction[0]
