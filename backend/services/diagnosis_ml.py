f61d70714f2bb4f4d6f0617ac4971f3080d81cc2
Vinay Sharma <vinayiet435@gmail.com>
2025-05-06 23:17:12 +0530

    added Label encoder to diagnosis file

--- backend/services/diagnosis_ml.py
+++ backend/services/diagnosis_ml.py
@@ -5,10 +5,11 @@ from typing import List
 # Get the absolute path to the model file based on this script's location
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MODEL_PATH = os.path.join(BASE_DIR, 'diagnosis_model.pkl')
-
+LABEL_ENCODER = os.path.join(BASE_DIR, 'label_encoder.pkl')
 # Load the model using full path
 model = joblib.load(MODEL_PATH)
-print('Prediction model is loaded')
+encoder = joblib.load(LABEL_ENCODER)
+print('Prediction model is loaded!')
 
 SYMPTOM_COLS = [
     'fever', 'headache', 'nausea', 'vomiting', 'fatigue',
@@ -22,4 +23,5 @@ def encode_symptoms(input_symptoms: List[str]) -> List[int]:
 def predict_disease(symptoms: List[str]) -> str:
     features = encode_symptoms(symptoms)
     prediction = model.predict([features])
-    return prediction[0]
+    disease_name = encoder.classes_[prediction[0]]
+    return disease_name
